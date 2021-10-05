24c4797c0afa8aa7f2da8b0e65f156e0e86de004
Mohammadreza Borjian <mborjian82@gmail.com>
2021-10-05 21:26:52 +0330

    Initial commit

---
+++ app/src/main/java/ir/mapir/MainActivity.kt
@@ -0,0 +1,58 @@
+package ir.mapir
+
+import android.os.Bundle
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
+import android.preference.PreferenceManager
+import androidx.appcompat.app.AppCompatActivity
+import org.osmdroid.api.IMapController
+import org.osmdroid.config.Configuration
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.CopyrightOverlay
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mapView: MapView
+    private lateinit var controller: IMapController
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Configuration.getInstance().userAgentValue = packageName
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
+        val policy = ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+        mapView = findViewById(R.id.map)
+        controller = mapView.controller
+        setMapViewConfig()
+
+
+    }
+    private fun setMapViewConfig() {
+        mapView.overlayManager.tilesOverlay.loadingBackgroundColor = resources.getColor(R.color.white)
+        mapView.overlayManager.tilesOverlay.loadingLineColor = resources.getColor(R.color.white)
+        mapView.setTileSource(
+            MapIrTileProvider(
+                "Map.ir",
+                0,
+                22,
+                256,
+                ".png",
+                arrayOf("https://map.ir/shiveh")
+            )
+        )
+        mapView.setMultiTouchControls(true)
+        mapView.maxZoomLevel = 19.toDouble()
+        mapView.minZoomLevel = 12.toDouble()
+        controller.setZoom(15.toDouble())
+        val startPoint = GeoPoint(34.639942, 50.875942)
+        controller.setCenter(startPoint)
+        mapView.isTilesScaledToDpi = true
+        val mCopyrightOverlay = CopyrightOverlay(this)
+        mCopyrightOverlay.setTextColor(resources.getColor(R.color.black))
+        mCopyrightOverlay.setOffset(20, 40)
+        mapView.overlays.add(mCopyrightOverlay)
+    }
+
+}
---
+++ app/src/main/java/ir/mapir/MapIrTileProvider.kt
@@ -0,0 +1,81 @@
+package ir.mapir
+
+import org.osmdroid.tileprovider.tilesource.OnlineTileSourceBase
+import org.osmdroid.util.MapTileIndex
+import java.net.MalformedURLException
+import java.net.URL
+import java.util.*
+
+class MapIrTileProvider(
+    aName: String?,
+    aZoomMinLevel: Int,
+    aZoomMaxLevel: Int,
+    aTileSizePixels: Int,
+    aImageFilenameEnding: String?,
+    aBaseUrl: Array<out String>?
+) : OnlineTileSourceBase(
+    aName,
+    aZoomMinLevel,
+    aZoomMaxLevel,
+    aTileSizePixels,
+    aImageFilenameEnding,
+    aBaseUrl
+) {
+    private val MINX = 0
+    private val MAXX = 1
+    private val MINY = 2
+    private val MAXY = 3
+    private val TILE_ORIGIN = doubleArrayOf(-20037508.34789244, 20037508.34789244)
+    var OSGEO_WMS = "https://map.ir/shiveh?" +
+            "service=WMS" +
+            "&version=1.1.0" +
+            "&EXCEPTIONS=application/vnd.ogc.se_inimage" +
+            "&request=GetMap" +
+            "&layers=Shiveh:Shiveh" +
+            "&width=256" +
+            "&height=256" +
+            "&srs=EPSG:3857" +
+            "&format=image/png" +
+            "&bbox=%f,%f,%f,%f" +
+            "&x-api-key="
+
+    private val apiKey =
+        "your token"
+
+    override fun getTileURLString(pMapTileIndex: Long): String {
+        val x = MapTileIndex.getX(pMapTileIndex)
+        val y = MapTileIndex.getY(pMapTileIndex)
+        val z = MapTileIndex.getZoom(pMapTileIndex)
+
+
+        val bbox: Array<Double?> = getBoundingBox(x, y, z)
+        val s = String.format(
+            Locale.US, OSGEO_WMS,
+            bbox[MINX], bbox[MINY], bbox[MAXX], bbox[MAXY]
+        ) + apiKey
+        val url: URL
+        url = try {
+            URL(s)
+        } catch (e: MalformedURLException) {
+            throw AssertionError(e)
+        }
+        return url.toString()
+    }
+
+    private fun getBoundingBox(x: Int, y: Int, zoom: Int): Array<Double?> {
+        val tileSize = 20037508.34789244 * 2 / Math.pow(
+            2.0,
+            zoom.toDouble()
+        )
+        val bbox = arrayOfNulls<Double>(4)
+        bbox[MINX] = TILE_ORIGIN.get(0) + x * tileSize
+        bbox[MINY] = TILE_ORIGIN.get(1) - (y + 1) * tileSize
+        bbox[MAXX] = TILE_ORIGIN.get(0) + (x + 1) * tileSize
+        bbox[MAXY] = TILE_ORIGIN.get(1) - y * tileSize
+        return bbox
+    }
+
+    override fun getCopyrightNotice(): String? {
+        return "© your copyRight"
+    }
+}
